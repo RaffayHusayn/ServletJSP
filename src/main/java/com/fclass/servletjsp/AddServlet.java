32ec766c8d85ffaaa356a63d0319ea77e0901e7b
Raffay Syed <raffayhusayn@gmail.com>
2021-10-15 17:01:10 -0400

    Change service method of servlet to doPost()
    --Added functionality to the server to only accept Post HTTP request

--- src/main/java/com/fclass/servletjsp/AddServlet.java
+++ src/main/java/com/fclass/servletjsp/AddServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1+ num2;
