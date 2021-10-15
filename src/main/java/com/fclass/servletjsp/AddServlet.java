e608dfee369d0b2ddd358ff24a418645648c94ae
Raffay Syed <raffayhusayn@gmail.com>
2021-10-15 19:50:46 -0400

    Add a servlet which takes data from another servlet and process it
    -- Added SqServlet which takes data from AddServlet
    -- Squares the value it receives and returns the output

--- src/main/java/com/fclass/servletjsp/AddServlet.java
+++ src/main/java/com/fclass/servletjsp/AddServlet.java
@@ -3,20 +3,22 @@ package com.fclass.servletjsp;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 
-    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1+ num2;
 
-        PrintWriter out = res.getWriter();
-        out.println(result);
-
+        req.setAttribute("result-attribute", result);
+        RequestDispatcher rd = req.getRequestDispatcher("square");//param : url of the servlet it's calling
+        rd.forward(req, res);
 
     }
 
---
+++ src/main/java/com/fclass/servletjsp/SqServlet.java
@@ -0,0 +1,19 @@
+package com.fclass.servletjsp;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SqServlet extends HttpServlet {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException{
+
+        int noToSquare = (int) req.getAttribute("result-attribute");
+        noToSquare = noToSquare*noToSquare;
+        PrintWriter out = res.getWriter();
+        out.println(noToSquare);
+
+    }
+}
