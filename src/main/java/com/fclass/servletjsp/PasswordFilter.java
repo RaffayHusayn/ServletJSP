5dfb56ceec0b6c775c2e58b398275bd33928f95d
Raffay Syed <raffayhusayn@gmail.com>
2021-10-24 03:40:36 -0400

    Add a Filter which test the password, if password is shorter than 3 chars, it gives an error

---
+++ src/main/java/com/fclass/servletjsp/PasswordFilter.java
@@ -0,0 +1,28 @@
+package com.fclass.servletjsp;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebFilter(filterName = "PasswordFilter")
+@WebFilter("/jstlsqljsp")
+public class PasswordFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        String password = request.getParameter("password");
+        if(password.length() > 3) {
+            chain.doFilter(request, response);
+        }else{
+            out.println("password too short");
+        }
+    }
+}
